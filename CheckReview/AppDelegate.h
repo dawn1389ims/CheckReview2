ba3a8a179fb5865c5a0453fb20504dc96a9b884e
Zhiqiang <Muling@RuandeMacBook-Air-2.local>
2014-07-21 16:42:29 +0800

    Initial Commit

---
+++ CheckReview/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CheckReview
+//
+//  Created by Zhiqiang on 7/21/14.
+//  Copyright (c) 2014 Muling. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CheckReview/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  CheckReview
+//
+//  Created by Zhiqiang on 7/21/14.
+//  Copyright (c) 2014 Muling. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
